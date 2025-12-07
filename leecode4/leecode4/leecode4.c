77f9056b12199e227bffa672bf8d628899a8193c
Charmmmy <2127137114@qq.com>
2025-12-07 18:09:45 +0800

    Add files via upload

---
+++ leecode4/leecode4/leecode4.c
@@ -0,0 +1,20 @@
+﻿
+int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize)
+{
+    int* arr = malloc(numsSize * sizeof(int));
+    for (long long i = 0; i < numsSize; i++)
+    {
+        long long a = 0;
+        for (long long j = 0; j < numsSize; j++)
+        {
+            if (nums[i] > nums[j])
+            {
+                a++;
+            }
+        }
+        arr[i] = a;
+        printf("%d", arr[i]);
+    }
+    *returnSize = numsSize;
+    return arr;
+}
